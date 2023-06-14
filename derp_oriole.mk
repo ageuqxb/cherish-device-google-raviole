0d7d9e82efe87319ebe4a74890d02fce9f32e8de
Michael Bestas <mkbestas@lineageos.org>
2023-06-14 23:14:34 +0300

    Add fingerprint/build description from TQ3A.230605.010

--- derp_oriole.mk
+++ derp_oriole.mk
@@ -38,3 +38,9 @@ TARGET_SYSTEM_PROP := device/google/raviole/system.prop
 PRODUCT_NAME := derp_oriole
 PRODUCT_MODEL := Pixel 6
 PRODUCT_BRAND := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=oriole \
+    PRIVATE_BUILD_DESC="oriole-user 13 TQ3A.230605.010 10121037 release-keys"
+
+BUILD_FINGERPRINT := google/oriole/oriole:13/TQ3A.230605.010/10121037:user/release-keys
