fd4db6d443d085f5c2ad9f06b0928e6b50d5a901
Barba_Tech <nonoqxb@gmail.com>
2023-09-03 08:14:43 -0300

    Raviole: add Lineage Health to lineage-hals.mk

--- lineage/lineage-hals.mk
+++ lineage/lineage-hals.mk
@@ -3,3 +3,6 @@ $(call inherit-product, device/google/raviole/lineage/hals/powershare/device.mk)
 
 #Touch
 $(call inherit-product, device/google/raviole/lineage/hals/touch/device.mk)
+
+#Lineage Health
+$(call inherit-product, device/google/raviole/lineage/hals/lineage_health/device.mk)
